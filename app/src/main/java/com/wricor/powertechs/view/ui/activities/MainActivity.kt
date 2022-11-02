3aeec93c8719e87e4c214b44887db5dd46f2d839
William Jammirlhey Rico Ruiz <jammer927@gmail.com>
2022-11-02 11:41:36 -0500

    Configuración inicial

---
+++ app/src/main/java/com/wricor/powertechs/view/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wricor.powertechs.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wricor.powertechs.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
