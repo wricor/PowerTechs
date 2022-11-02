a3e729ee17e8fe0ccf5ccff2bdcf696336c6c765
William Jammirlhey Rico Ruiz <jammer927@gmail.com>
2022-11-02 15:35:27 -0500

    Creación de login y signup.

--- app/src/main/java/com/wricor/powertechs/view/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/wricor/powertechs/view/ui/activities/LoginActivity.kt
@@ -1,12 +1,25 @@
 package com.wricor.powertechs.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.wricor.powertechs.R
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var loginbutton: Button
+    lateinit var signupbutton: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginbutton = findViewById(R.id.btn_login_ingresar)
+        signupbutton = findViewById(R.id.txt_login_registro)
+        loginbutton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+        signupbutton.setOnClickListener {
+            startActivity(Intent(this, SignupActivity::class.java))
+        }
     }
 }
