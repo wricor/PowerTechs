1d0b78b310b1d8bdb878addf56394d6d8b6ab168
William Jammirlhey Rico Ruiz <jammer927@gmail.com>
2022-11-02 14:26:45 -0500

    Creación del splash.

---
+++ app/src/main/java/com/wricor/powertechs/view/ui/activities/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.wricor.powertechs.view.ui.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.wricor.powertechs.R
+import com.wricor.powertechs.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySplashBinding
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.animationView.setAnimation(R.raw.powertechsanim)
+        binding.animationView.playAnimation()
+
+        handler = Handler(Looper.myLooper()!!)
+
+        handler.postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
+    }
+}
